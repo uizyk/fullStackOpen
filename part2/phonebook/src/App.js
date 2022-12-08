36097b05d3e52813e2d80e634cc053165613d187
Sam <106921485+uizyk@users.noreply.github.com>
2022-12-08 14:09:40 +0900

    update: added existing name alert

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,47 +1,64 @@
 import { useState } from 'react'
 
-const Person = ({person}) => {
-  return(
-    <p>{person.name}</p>
-  )
-}
+import Person from './components/Person'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  ]);
 
-  const addName = (e) => {
-    console.log(e.target);
+  const [newName, setNewName] = useState('');
+
+  const [filter, setFilter] = useState(true);
+
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
   } 
 
-  const addPerson = (e) => {
-    e.preventDefault()
-    setPersons(e.target.value);
+  const existingName = persons.map(person => person.name);
+
+  const addName = (e) => {
+    e.preventDefault();
+    const nameObj = {
+      name: '',
+      number: ''
+    }
+    if (existingName.includes(newName))
+    {
+      alert(`${newName} is already added to phonebook`)
+    } else
+    {
+      nameObj.name = newName;
+      setPersons(persons.concat(nameObj));
+      setNewName('');
+    }
   }
+
+  // console.log(persons);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={handleNameChange}>
         <div>
-          name: <input value={newName} onChange={addName}/>
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit" onClick={addPerson}>add</button>
+          <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
-          <Person person={person.name} />
+        {persons.map(person =>
+          <Person 
+          personName={person.name}
+          personNumber={person.number}
+          key={person.name} 
+          />
         )}
       </ul>
 
-      ...
-      <div>debug: {newName}</div>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = ({personName}) => {
+    return(
+        <p>{personName}</p>
+    )
+}
+
+export default Person
