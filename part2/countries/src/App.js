eecd538c80d2969a7abc131c97c3e58cc2619226
Sam <106921485+uizyk@users.noreply.github.com>
2022-12-09 14:38:15 +0900

    update: countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 function App() {
-  return (
+  
+  const [countries, setCountries] = useState([]);
+  const [filter , setFilter] = useState('');
+
+  const filterInputChange = (e) => {
+    setFilter(e.target.value);
+  }
+
+  const filtered = !filter
+  ? countries
+  : countries.filter((countries) => 
+    countries.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  useEffect(() => {
+
+    axios.get('https://restcountries.com/v3.1/all').then(response =>{
+      const countriesData = response.data
+      console.log(countriesData);
+    })
+
+  }, [])
     
+
+  return (
+    <div>
+      find countries <input type='text' />
+    </div>
   )
 
 }
