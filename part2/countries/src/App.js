3f029b0c76bdd37766b99b52e892b872c74d0bd7
Sam <106921485+uizyk@users.noreply.github.com>
2022-12-10 10:24:10 +0900

    update: display country names and filter functionality

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,36 +1,45 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import Country from './components/Country'
+
 
 
 function App() {
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then(response =>{
+      const countriesData = response.data
+      setCountries(countriesData);
+      console.log(countriesData);
+    })
+  }, [])
+
   
   const [countries, setCountries] = useState([]);
   const [filter , setFilter] = useState('');
 
   const filterInputChange = (e) => {
     setFilter(e.target.value);
+    console.log(e.target.value);
   }
 
   const filtered = !filter
   ? countries
-  : countries.filter((countries) => 
-    countries.name.toLowerCase().includes(filter.toLowerCase())
+  : countries.filter((country) => 
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
   );
 
-  useEffect(() => {
-
-    axios.get('https://restcountries.com/v3.1/all').then(response =>{
-      const countriesData = response.data
-      console.log(countriesData);
-    })
-
-  }, [])
     
 
   return (
     <div>
-      find countries <input type='text' />
+      find countries <input type='text' onChange={filterInputChange}/>
+      <ul>
+      {filtered.map(country =>
+        <Country countryName={country.name.common} key={country.name.official}/>
+      )}
+      </ul>
     </div>
   )
 
---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Country = ({countryName}) => {
+    return(
+        <p>{countryName}</p>
+    )
+}
+
+export default Country; 
