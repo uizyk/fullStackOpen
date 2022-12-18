62521c5c1731a8444e477af3e5510a9b82e10097
Sam <106921485+uizyk@users.noreply.github.com>
2022-12-18 12:01:20 +0900

    edit

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let persons = [
     { 
@@ -23,11 +26,37 @@ let persons = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+
+app.get('/info', (request, response) => {
+  numOfPeople = persons.length;
+  response.send(
+    `<p>Phonebook has info for ${numOfPeople} people</p>
+    <p></p>
+    `
+    );
+  })
+
+
+  // app.post('/info', (request, response) => {
+  //   const info = request.body
+  //   response.send(<p>hello</p>)
+  // })
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person){
+    response.json(person)
+  } else {
+    response.status(404).end();
+  }
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
