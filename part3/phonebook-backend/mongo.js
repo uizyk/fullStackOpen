f739a9eed78d943895ee694fe144f26f8fafb974
Sam <uizykim@gmail.com>
2023-08-23 14:12:04 +0900

    save person to db

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -7,14 +7,25 @@ if (process.argv.length<3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://uizykim:${password}@cluster0.idegc5r.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://uizykim:${password}@cluster0.idegc5r.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
+
 const personSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+    name: String,
+    number: String,
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema, 'persons');
+
+const person = new Person({
+    name: 'Save test',
+    number: '123-456-7890',
+})
+
+person.save().then(result => {
+    console.log('person saved!');
+    mongoose.connection.close();
+})
