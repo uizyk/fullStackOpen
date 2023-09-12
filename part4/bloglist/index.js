d02b6735c32d1598d0e04a346ec150b532fe2399
Sam <uizykim@gmail.com>
2023-09-12 13:35:16 +0900

    add mongojs

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,13 +1,30 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
-    response.end('Hello World')
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+blog
+    .save()
+    .then(result => {
+        response.status(201).json(result)
+    })
 })
 
-const PORT = 3001
-
-app.listen(PORT)
-
-console.log(`Server running on port ${PORT}`)
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ part4/bloglist/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const mongoUrl = `mongodb+srv://uizykim:${password}@cluster0.5bn1ycq.mongodb.net/?retryWrites=true&w=majority`
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect(mongoUrl);
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+
+const blog = new Blog({
+    title: process.argv[3],
+    author: process.argv[4],
+    url: process.argv[5],
+    likes: process.argv[6]
+});
+
+/// / Save a blog
+if (process.argv.length > 3) {
+  blog.save().then((result) => {
+    console.log(`added ${blog.title} to bloglist`);
+    mongoose.connection.close();
+  });
+}
+
+/// / Find all blogs
+if (process.argv.length === 3) {
+  Blog.find({}).then((result) => {
+    result.forEach((blog) => {
+      console.log(blog);
+    });
+    mongoose.connection.close();
+  });
+}
